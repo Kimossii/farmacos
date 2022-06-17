d7f74ddde34f9837b3219493e3c90fb9f1f96e72
Kimossii <107686134+Kimossii@users.noreply.github.com>
2022-06-17 13:44:42 +0100

    Add files via upload

---
+++ farmacos/src/test/java/springbootthymeleafwebappp1/farmacos/FarmacosApplicationTests.java
@@ -0,0 +1,13 @@
+package springbootthymeleafwebappp1.farmacos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmacosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
